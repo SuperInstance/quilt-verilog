8b04557f02f3bfb4754170ebffbe09dc61b4391e
Seed Agent <seed@quilt-verilog.local>
2026-09-03 14:27:07 -0800

    hygiene: untrack verilator build artifacts (cosim/obj); ignore obj/out/vlt outputs

--- cosim/obj/n6_g0/spin19_tb.d
+++
@@ -1,6 +0,0 @@
-spin19_tb.o: ../../../cosim/vlt/spin19_tb.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h Vq_wall_gate.h
